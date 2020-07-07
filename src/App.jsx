2ce828ace460e059a0861dd7dbaaca35798c25af
deithwenfjell <david.klinovsky@gmail.com>
2020-07-07 09:39:15 +0200

    added states for creating and deleting notes and upgraded styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "./notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return <div>
         <Header/>
-        {notes.map( x => <Note key={x.key} header={x.title} text={x.content}/> )}
+        <CreateArea onAdd={addNote} />
+        {notes.map((noteItem, index) => {
+            return (
+                <Note
+                    key={index}
+                    id={index}
+                    header={noteItem.title}
+                    text={noteItem.content}
+                    onDelete={deleteNote}
+                />
+            );
+        })}
         <Footer/>
     </div>
 }
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 function Note(props) {
+
+    function handleClick() {
+        props.onDelete(props.id);
+    }
+
     return <div className="note">
         <h1>{props.header}</h1>
         <p>{props.text}</p>
+        <button onClick={handleClick}>DELETE</button>
     </div>
 }
 
